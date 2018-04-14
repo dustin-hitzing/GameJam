b35d80e4dd5f07b6499e38f56711204f7f74e094
DESKTOP-GOHM7OO\GuestUser <dustinhitzing@gmail.com>
2018-04-14 16:53:55 -0400

    added camera scripts and movement scripts

---
+++ Assets/Scripts/ZoomOnMove.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoomOnMove : MonoBehaviour
+{
+    public Camera mainCamera;
+    public PlayerPlatformerController player;
+    public float cameraMaxSize = 20f;
+    public float cameraStartSize = 15f;
+    public float cameraAdjustmentSpeed = 0.5f;
+    public CameraFollow cameraFollow;
+    // Use this for initialization
+    private void Start()
+    {
+        player = GetComponent<PlayerPlatformerController>();
+        mainCamera.orthographicSize = cameraStartSize;
+        cameraFollow = mainCamera.GetComponent<CameraFollow>();
+    }
+    // Update is called once per frame
+    void Update ()
+    {
+        var xDiff = Mathf.Abs(cameraFollow.xDifference);
+        var yDiff = Mathf.Abs(cameraFollow.yDifference);
+		if (Mathf.Abs(player.move.x) > 0 && xDiff > 3 || yDiff > 3)
+        {
+            if (mainCamera.orthographicSize < cameraMaxSize)
+            {
+                mainCamera.orthographicSize += cameraAdjustmentSpeed;
+            } 
+        }
+        else
+        {
+            if (mainCamera.orthographicSize > cameraStartSize)
+            {
+                mainCamera.orthographicSize -= cameraAdjustmentSpeed;
+            }
+            
+        }
+	}
+}
