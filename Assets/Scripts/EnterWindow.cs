0a356127aa3f64e9d72078369c2c59485024691b
Kara Todd <kara@voxel-fox.com>
2018-04-15 06:08:21 -0400

    remove debug

--- Assets/Scripts/EnterWindow.cs
+++ Assets/Scripts/EnterWindow.cs
@@ -5,10 +5,8 @@ using UnityEngine.SceneManagement;
 
 public class EnterWindow : MonoBehaviour {
 	private void OnTriggerEnter2D(Collider2D other) {
-		Debug.Log("hit window");
 		if (other.gameObject.layer == 10)
 		{
-			Debug.Log("Win!");
 			SceneManager.LoadScene("Win");
 		}
 	}
